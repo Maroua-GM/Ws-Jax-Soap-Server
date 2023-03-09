8ca43d92f2bdb0fa40806c98af7c3e8ecf07ca05
BENKHEDIR Maroua <maroua2012@gmail.com>
2023-03-09 09:23:06 +0100

    mettre a jour les web services

--- src/main/java/fr/doranco/jaxws/dao/EtudiantDao.java
+++ src/main/java/fr/doranco/jaxws/dao/EtudiantDao.java
@@ -1,6 +1,7 @@
 package fr.doranco.jaxws.dao;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -14,6 +15,11 @@ public class EtudiantDao implements IEtudiantDao {
 	public Etudiant addEtudiant(Etudiant etudiant) throws Exception {
 		Session session = null;
 		Transaction tx = null;
+		if (etudiant == null || etudiant.getNom() == null || etudiant.getNom().trim().isEmpty()
+				|| etudiant.getPrenom() == null || etudiant.getPrenom().trim().isEmpty()
+				|| etudiant.getSpecialite() == null || etudiant.getSpecialite().trim().isEmpty()) {
+			throw new IllegalArgumentException("on accepte pas une valeur null");
+		}
 
 		try {
 			session = DataSourceConnexion.getSession();
@@ -38,6 +44,9 @@ public class EtudiantDao implements IEtudiantDao {
 		try {
 			session = DataSourceConnexion.getSession();
 			etudiants = session.createQuery("from Etudiant").list();
+			if (etudiants.isEmpty()) {
+				throw new NoSuchElementException("la liste est vide");
+			}
 		} finally {
 			if (session != null && session.isOpen()) {
 				session.close();
@@ -58,8 +67,7 @@ public class EtudiantDao implements IEtudiantDao {
 			etudiant = (Etudiant) session.createQuery("select e from Etudiant e where id=?1").setParameter(1, id)
 					.getSingleResult();
 		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
+			throw new NoSuchElementException("l'etudiant n'existe pas");
 		} finally {
 			if (session != null && session.isOpen()) {
 				session.close();
